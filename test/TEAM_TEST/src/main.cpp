7c812a84f21c67b2ed470c040a3a53bc72ce56b8
masashi37 <bantan-gp-mk37@ezweb.ne.jp>
2015-09-26 13:39:12 +0900

    チーム制作時のGit練習

---
+++ test/TEAM_TEST/src/main.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include "lib/framework.hpp"
+#include <iostream>
+
+
+enum Window {
+	WIDTH = 512,
+	HEIGHT = 512
+};
+
+enum SceneName {
+	TITLE,
+	GAME,
+};
+
+
+int main() {
+
+	AppEnv env(Window::WIDTH, Window::HEIGHT, false, true);
+
+	while (env.isOpen()) {
+
+		env.begin();
+
+		env.end();
+
+	}
+
+}
